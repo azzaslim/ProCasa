b1444341624532f787d6c54db3d564978dd2bdef
azzaslim <azzaslim3@gmail.com>
2024-07-21 18:04:40 +0100

    crete login slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,75 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react';
 
-const Home = () => {
+const images = [
+  '/assets/img/slide-1.jpg',
+  '/assets/img/slide-2.jpg',
+  '/assets/img/slide-3.jpg',
+];
+
+const Home: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <>{ /*
-      This example requires some changes to your config:
-      
-      ```
-      // tailwind.config.js
-      module.exports = {
-        // ...
-        plugins: [
-          // ...
-          require('@tailwindcss/forms'),
-        ],
-      }
-      ```
-    */ }
- { /*
-    This example requires updating your template:
-  
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  */ }
-<div className='flex items-center justify-center h-screen'>
+    <div className="relative h-screen w-screen">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`absolute inset-0 transition-opacity duration-1000 ${index === currentIndex ? 'opacity-100' : 'opacity-0'}`}
+          style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+        ></div>
+      ))}
 
-  <div className='w-50 h-46 px-2 mt-8 mb-4 overflow-hidden rounded-3xl bg-red text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 mx-auto'>
-  <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
-   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-     <img className="mx-auto h-40 w-auto" src="/assets/img/ProCasa-Logo.png"  />
-     <h2 className=" text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
-   </div>
- 
-   <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-     <form className="space-y-6" action="#" method="POST">
-       <div>
-         <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-         <div className="mt-2">
-           <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-         </div>
-       </div>
- 
-       <div>
-         <div className="flex items-center justify-between">
-           <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-           <div className="text-sm">
-             <a href="#" className="font-semibold text-red-400 hover:text-red-400">Forgot password?</a>
-           </div>
-         </div>
-         <div className="mt-2">
-           <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-         </div>
-       </div>
- 
-       <div>
-         <button type="submit" className="flex w-full justify-center rounded-md bg-red-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-text-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-       </div>
-     </form>
- 
-     <p className="mt-10 text-center text-sm text-gray-500">
-      <a href="/signup" className="text-red-400 hover:text-red-400" >Not a member?</a> 
-     </p>
-   </div>
- </div>
- </div> </div>
- </>
- 
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="w-5/12 px-2 mt-8 mb-4 overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5 mx-auto">
+          <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <img className="mx-auto h-40 w-auto" src="/assets/img/ProCasa-Logo.png" alt="ProCasa Logo" />
+              <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                  <div className="mt-2">
+                    <input id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold text-red-400 hover:text-red-400">Forgot password?</a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  </div>
+                </div>
+
+                <div>
+                  <button type="submit" className="flex w-full justify-center rounded-md bg-red-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-text-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                <a href="/signup" className="text-red-400 hover:text-red-400">Not a member?</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default Home
+export default Home;
